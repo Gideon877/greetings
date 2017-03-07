4310c6ff1a839e8d78827e4ca986d177a204f09d
gideon877 <tblar84@gmail.com>
2017-03-07 15:56:41 +0200

    Edited greet.js and index.html

--- greet.js
+++ greet.js
@@ -1,26 +1,31 @@
-/*var count = document.querySelector('#counter');
-var clicks = 0;
-*/
+var greeting = document.getElementById('display');
+var name1 = document.getElementById('user_input');
+
+
 function showInput(){
+	if (name1.value.length > 0 || name1.value === null){
 
-	if (document.getElementById('english').checked) {
-    document.getElementById('display').innerHTML =
-                   "Hello, " + document.getElementById("user_input").value + ".";
-                   document.getElementById("user_input").value = "";
-        var container  = clickCounter();
+		if (document.getElementById('english').checked) {
+	    greeting.innerHTML =
+	                   "Hello, " + name1.value + ".";
+	                   name1.value = "";
+	        var container  = clickCounter();
 
-		} else if (document.getElementById('setswana').checked) {
-  		document.getElementById('display').innerHTML =
-                     "Dumela, " + document.getElementById("user_input").value + ".";
-                     document.getElementById("user_input").value = "";
-          var container  = clickCounter();
+			} else if (document.getElementById('setswana').checked) {
+	  		greeting.innerHTML =
+	                     "Dumela, " + name1.value + ".";
+	                     name1.value = "";
+	          var container  = clickCounter();
 
-		} else if (document.getElementById('afrikaans').checked) {
-		document.getElementById('display').innerHTML =
-                   "Hallo, " + document.getElementById("user_input").value + ".";
-                   document.getElementById("user_input").value = "";
-          var container  = clickCounter();
-
+			} else if (document.getElementById('zulu').checked) {
+				greeting.innerHTML =
+	                   "Sawubona, " + name1.value + ".";
+	                   name1.value = "";
+	          var container  = clickCounter();
+			}
+		}
+		else{
+			greeting.innerHTML = "Please enter your name";
 		}
 };
 
@@ -36,3 +41,19 @@ function clickCounter() {
         document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
     }
 }
+
+var x = document.getElementById("myBtn");
+x.addEventListener("click", resetClear);
+
+function resetClear(){
+	alert ("Greetings counter deleted!");
+	sessionStorage.clickcount = 0;
+
+}
+
+var namesGreeted = {};
+/*
+if (namesGreeted[name1] === undefined){
+	var clickCounter++;
+}
+*/
