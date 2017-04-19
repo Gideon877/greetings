e9a5b3e678e577434710f9522b37866133fae011
gideon877 <tblar84@gmail.com>
2017-04-19 11:05:29 +0200

    modification and improvement to an existing code

--- greet.js
+++ greet.js
@@ -3,13 +3,13 @@ Coder	: Thabang Gideon Magaola
 About	: This is a web page that allows you to enter a name and when a button is pressed display a greeting
 				* keep track of how many times people were greeted
 				* greet people in more than one language
-
 */
 var greeting = document.getElementById('display');
 var name1 = document.getElementById('user_input');
 
-var namesGreeted = {};
+var namesGreeted = {}; //this variable will store all the new names.
 
+//This 'for loop'
 for (var i=0; i<namesGreeted.length; i++){
 }
 
@@ -21,43 +21,36 @@ function showInput(){
 		namesGreeted[name1.value] = 1;
 
 		if (document.getElementById('english').checked) {
-	    greeting.innerHTML =
-	                   "Hello, " + name1.value + ".";
-	                   name1.value = "";
-	        var container  = clickCounter();
+	    	greeting.innerHTML = "Hello, " + name1.value + ".";
+			name1.value = "";
+	       	var container  = clickCounter();
 
 			} else if (document.getElementById('setswana').checked) {
-	  		greeting.innerHTML =
-	                     "Dumela, " + name1.value + ".";
-	                     name1.value = "";
-	          var container  = clickCounter();
+	  		greeting.innerHTML = "Dumela, " + name1.value + ".";
+			name1.value = "";
+	       	var container  = clickCounter();
 
 			} else if (document.getElementById('zulu').checked) {
-				greeting.innerHTML =
-	                   "Sawubona, " + name1.value + ".";
-	                   name1.value = "";
-	          var container  = clickCounter();
+			greeting.innerHTML = "Sawubona, " + name1.value + ".";
+	       	name1.value = "";
+         	var container  = clickCounter();
 			}
 		}
 
-		else if (name1.value.length > 0 && namesGreeted[name1.value] !== undefined){
-			if (document.getElementById('english').checked) {
-				greeting.innerHTML =
-											 "Hello " + name1.value + ", welcome back.";
-											 name1.value = "";
+	else if (name1.value.length > 0 && namesGreeted[name1.value] !== undefined){
+		if (document.getElementById('english').checked) {
+			greeting.innerHTML = "Hello " + name1.value + ", welcome back.";
+			name1.value = "";
 
-				} else if (document.getElementById('setswana').checked) {
-					greeting.innerHTML =
-												 "Dumela " + name1.value + ", rea ho amohela.";
-												 name1.value = "";
+		} else if (document.getElementById('setswana').checked) {
+			greeting.innerHTML = "Dumela " + name1.value + ", rea ho amohela.";
+			name1.value = "";
 
-				} else if (document.getElementById('zulu').checked) {
-					greeting.innerHTML =
-											 "Sawubona " + name1.value + ", siyakwamukela.";
-											 name1.value = "";
-
-				}
+		} else if (document.getElementById('zulu').checked) {
+			greeting.innerHTML = "Sawubona " + name1.value + ", siyakwamukela.";
+			name1.value = "";
 		}
+	}
 };
 
 function clickCounter() {
@@ -67,7 +60,7 @@ function clickCounter() {
         } else {
             localStorage.clickcount = 1;
         }
-        document.getElementById("result").innerHTML = "You have been greeted " + localStorage.clickcount + " time(s) in this session.";
+        document.getElementById("result").innerHTML = "Names greeted for this session: " + localStorage.clickcount;
     } else {
         document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
     }
@@ -77,14 +70,7 @@ var x = document.getElementById("myBtn");
 x.addEventListener("click", resetClear);
 
 function resetClear(){
-	//alert ("Greetings counter deleted!");
+//	alert ("Greetings counter deleted!");
 	localStorage.clickcount = 0;
-
+	//greeting.innerHTML = "You have been greeted " + localStorage.clickcount + " time(s) in this session.";
 }
-
-
-/*
-if (namesGreeted[name1] === undefined){
-	var clickCounter++;
-}
-*/
