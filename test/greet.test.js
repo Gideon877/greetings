b2a9776d075ccd9e620e0e5eca28c05532b20d00
gideon877 <tblar84@gmail.com>
2017-05-26 07:40:39 +0200

    Modification of existing code

--- test/greet.test.js
+++ test/greet.test.js
@@ -1,4 +1,4 @@
-describe('The languageCheck function', function() {
+describe('The greet function', function() {
 
     it('should greet Jen correctly in English', function() {
         if (english) {
@@ -6,46 +6,31 @@ describe('The languageCheck function', function() {
 
         }
     });
-    it('should greet Sibongile correctly in Zulu', function() {
-        if (zulu) {
-            assert.equal('Sawubona, Sibongile', languageCheck());
 
-        }
-    });
-    it('should greet Lerato correctly in Setswana', function() {
-        if (setswana) {
-            assert.equal('Dumela, Lerato', languageCheck());
+    it('should update the counter for a new Name'),
+        function(user_name) {
+            assert.equal(user_name === null)
+            if (namesGreeted[user_name] === undefined) {
+                namesGreeted[user_name] = 1;
 
-        }
-    });
-});
+                if (localStorage.clickcount) {
+                    localStorage.clickcount = Number(localStorage.clickcount) + 1;
+                } else {
+                    localStorage.clickcount = 1;
+                }
 
-describe('The clickCounter function', function() {
-
-    it('should count names greeted whenever a new name is entered.', function() {
-        if(typeof(Storage) !== "undefined") {
-            if (localStorage.clickcount) {
-                localStorage.clickcount = Number(localStorage.clickcount)+1;
-            } else {
-                localStorage.clickcount = 1;
             }
-            assert.equal("Names greeted for this session: 2", clickCounter());
-        } else {
-            assert.equal("Sorry, your browser does not support web storage...", clickCounter());
-        }
 
-    })
+        }
 });
 
-describe('The resetCounter function', function() {
+describe('The reset counter function', function() {
 
-    it('should clear the local storage and reset it to 0.', function() {
+    it('should reset greeting counter to 0', function() {
+
+        assert.equal(undefined)
         if (localStorage.clickcount >= 1) {
             localStorage.clickcount = 0;
-            assert.equal(0, resetCounter());
-        } else {
-            assert.equal("", resetCounter());
         }
-
-    })
+    });
 });
