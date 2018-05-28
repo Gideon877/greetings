d9d93856585b7c07764f43f979a8cda0d0d00a9f
Thabang Gideon Magaola <thabang@getwala.com>
2018-05-28 19:05:41 +0200

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,7 +1,7 @@
 function clickCounter() {
     if(typeof(Storage) !== "undefined") {
         if (localStorage.clickcount) {
-            localStorage.clickcount = Number(localStorage.clickcount)+1;
+            localStorage.clickcount = Number(localStorage.clickcount) + 1;
         } else {
             localStorage.clickcount = 1;
         }
